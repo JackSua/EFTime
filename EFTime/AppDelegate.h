480803aed43291d98066382a2dda3134082bee57
suzq <hekysu@qq.com>
2014-12-25 14:09:42 +0800

    Initial Commit

---
+++ EFTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EFTime
+//
+//  Created by suzq on 14/12/25.
+//  Copyright (c) 2014年 suzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EFTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EFTime
+//
+//  Created by suzq on 14/12/25.
+//  Copyright (c) 2014年 suzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
