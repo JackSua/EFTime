675e374abd75a0af9df2d6d2c468bc10683a033f
suzq <hekysu@qq.com>
2014-12-25 17:21:38 +0800

    (+) 添加设置页

---
+++ EFTime/Controllers/SettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewController.h
+//  EFTime
+//
+//  Created by suzq on 14/12/25.
+//  Copyright (c) 2014年 suzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewController : UITableViewController
+
+@end
